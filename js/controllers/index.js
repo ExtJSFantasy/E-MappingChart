7ed9205b7225b0e5d45498842070ed30b801b53f
xiangdeshan <xiangdeshan_120@sina.com>
2018-05-15 14:20:05 +0800

    调整
    调整

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -25,7 +25,12 @@ new Vue({
         inputNew: function() {
             mui.openWindow({
                 url: 'views/selectProcedure.html',
-                id: 'views/selectProcedure'
+                id: 'views/selectProcedure',
+                show:{
+                    autoShow:true,//页面loaded事件发生后自动显示，默认为true
+                    aniShow:'slide-in-right',//页面显示动画，默认为”slide-in-right“；
+                    duration:100//页面动画持续时间，Android平台默认100毫秒，iOS平台默认200毫秒；
+                }
             })
         }
     }
